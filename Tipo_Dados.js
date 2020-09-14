59d4e1a561322d7c0e692cee2ef26712d7955346
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-14 16:54:14 -0300

    AULAS AGILECODE

--- Tipo_Dados.js
+++ Tipo_Dados.js
@@ -1,10 +1,24 @@
-console.log( //<= para aparecer no console
-typeof 10,
-typeof "JavaScript",
-typeof true,
-typeof Symbol("interator"),
-typeof null,
-typeof undefined
+// console.log( //<= para aparecer no console
+// typeof 10,
+// typeof "JavaScript",
+// typeof true,
+// typeof Symbol("interator"),
+// typeof null,
+// typeof undefined
+// )
+
+console.log(
+    (10).toFixed(2),
+    ('JavaScript').replace('a', 3),
+    (Symbol("interator")).toString()
 )
 
 
+console.log(
+    typeof function sum(a, b) {return a + b},
+typeof {name: "Linus Torvalds"},
+typeof [1,2,3,4,5,6],
+typeof /[a-zA-Z]+/,
+typeof (new Date()),
+
+)

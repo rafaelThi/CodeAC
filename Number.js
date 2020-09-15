f4aaac0f2490f96c85fb41a492cc529c32be8ce1
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-15 10:47:51 -0300

    AULAS AGILECODE

---
+++ Number.js
@@ -0,0 +1,24 @@
+console.log(
+    10,
+9.9,
+0xFF,
+0b10,
+0o10,
+
+)
+
+console.log(
+    new Number(10),
+new Number(9.9),
+new Number(0xFF),
+new Number(0b10),
+new Number(0o10),
+
+)
+
+console.log(
+    (123.4).toExponential(10),
+(123.4).toFixed(10),
+(123.4).toPrecision(10),
+
+)

c05f8c69bf3d1a6f36ec3dff2120d4efc7f67ede
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-15 11:13:17 -0300

    AULAS AGILECODE

---
+++ Operadores_Numericos.js
@@ -0,0 +1,18 @@
+// console.log(
+//     2 + 2,
+// 8 - 3,
+// 4 * 5,
+// 9 / 3,
+// 7 % 2, 
+// )
+
+// console.log(
+// //    let result = 10, 
+//     result = 10,
+//     result += 2,
+//     result -= 5,
+//     result *= 8,
+//     result /= 2,
+//     result %= 6,
+
+// )

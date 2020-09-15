7056dc356dcabf1e43740a0e7789a7848c10e413
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-15 11:34:19 -0300

    AULAS AGILECODE

--- Operadores_Numericos.js
+++ Operadores_Numericos.js
@@ -16,3 +16,28 @@
 //     result %= 6,
 
 // )
+
+// console.log(
+//     // let result = 10,
+//     result = 10,
+// result++,
+// result,
+// ++result,
+// result--,
+// result,
+// --result,
+
+// )
+
+console.log('\n',
+4 | 3,'\n',//  OR  faz o calculo em binario, lebrar de eletrinica digital
+(4).toString(2).padStart(32,0),'\n',//mostra em binario
+(3).toString(2).padStart(32,0),'\n',
+3 & 1,'\n',// AND
+5 ^ 2,'\n',//XOR
+~2,'\n',//NOT
+4 << 2,'\n',//DESLOCAMENO DE BIT
+128 >> 1,'\n',//DESLOCAMENO DE BIT
+-2 >>> 1,'\n',//DESLOCAMENO DE BIT COM MUDANÇA DE SINAL
+
+)

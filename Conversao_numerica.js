d1d702b705d159ff5f3ee1e8d1caf71323975a69
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-15 15:00:53 -0300

    AULAS AGILECODE

---
+++ Conversao_numerica.js
@@ -0,0 +1,44 @@
+console.log(
+    Number("10"),
+Number("9.9"),
+Number("0xFF"),
+Number("0b10"),
+Number("0o10"),
+Number(),
+Number("JavaScript"),
+)
+
+console.log(
+    ~~"10",
++"10",
+"10" - 0,
+"10" * 1,
+"10" / 1,
+//pea a string 10 e converte em numero para poder fazer a operação
+)
+
+console.log("10" + 0) //entendeu como concatenação
+
+
+console.log((0xA).toString(10),
+(0b1010).toString(16),//hexa
+(0b1010),
+(010).toString(2),
+(10).toString(8),
+)
+
+console.log(parseInt("10", 10),
+parseInt("9.9", 10),
+parseInt("A", 16),
+parseInt("11", 2),
+parseInt("010", 8),
+)
+
+
+
+
+console.log(parseFloat("10"),
+parseFloat("2.5"),
+parseFloat("0xFF"),
+parseFloat("0b10"),
+)

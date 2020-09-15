1438557b0ca983d8b57a9e874573d856d81c4ef2
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-15 16:17:38 -0300

    AULAS AGILECODE

---
+++ ieee.js
@@ -0,0 +1,26 @@
+console.log(
+    0.1 + 0.2,
+666.7 - 666.6,
+33.3 * 3,
+12.2 / 0.1,
+
+)
+
+console.log(
+    1/0,
+Math.pow(10, 1000),
+Number.MAX_VALUE * 2,
+Math.log(0),
+-Number.MAX_VALUE * 2,
+)
+
+console.log(
+    10 * "JavaScript",
+0/0,
+Math.sqrt(-9),
+Math.log(-1),
+parseFloat("JavaScript"),
+NaN === NaN,
+isNaN(NaN),
+
+)

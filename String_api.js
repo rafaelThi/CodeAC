751ff7230aa7fb3ad7b2c3046afa4b689b7c098c
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-16 13:55:33 -0300

    AULAS AGILECODE

---
+++ String_api.js
@@ -0,0 +1,61 @@
+// console.log(
+// "JavaScript".length,
+// "PHP".indexOf("P"),
+// "PHP".lastIndexOf("P"),
+// "cobol".toUpperCase(),
+// "ALGOL".toLowerCase(),
+// )
+
+
+// console.log(
+//     "JavaScript".charAt(1), // mostra a posição
+// "JavaScript".charCodeAt(1),// mostra o codigo da letra
+// String.fromCharCode(97),//mostra a letra refenre ao codigo
+
+// )
+
+// console.log(
+//     "JavaScript".includes("Java"), //mostra tue se tiver na string
+// "Ruby".startsWith("R"),//mostra tue se tiver na string
+// "Erlang".endsWith("lang"),//mostra tue se tiver na string
+// );
+
+
+// console.log("C++".localeCompare("Ruby"),
+// "Python".localeCompare("Java"),
+// "JavaScript".localeCompare("JavaScript"),
+// )
+// // funciona com palavras com acentos 
+ 
+
+// console.log(
+//     "C++".match(/\+/g),
+// "Java".search(/a/),
+// "JavaScript".replace("Java", "Ecma"),
+// "JavaScript".replace(/a/g, 4),
+
+// )
+
+// console.log(
+//     "JavaScript".slice(0, 4),
+// "JavaScript".slice(4),
+// "JavaScript".slice(0, -6),
+// "JavaScript".slice(-6),
+// "C;Java;JavaScript;Ruby".split(";"),
+// "JavaScript".substring(0, 4),
+// "JavaScript".substring(4, 0),
+// "JavaScript".substring(4),
+
+// )
+
+console.log(
+    "Java".concat("Script"),
+"Data".concat("Flex"),
+"Script".padStart(100, "Java"),
+"C".padEnd(30, "+"),
+"C".concat("+".repeat(20)),
+"        Self      ".trim(),//tira espaco dos lados
+" Scheme ".trimLeft(),//tira espaco do lado
+" Perl ".trimRight(),//tira espaco do lado
+
+)

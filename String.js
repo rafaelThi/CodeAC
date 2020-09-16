1f9824f2915d6844bcb7d97dbb3fead573a79098
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-16 09:26:40 -0300

    AULA AGILECODE

---
+++ String.js
@@ -0,0 +1,43 @@
+// console.log(
+// 'JavaScript',
+// "JavaScript",
+// `JavaScript`,
+// )
+// console.log('JavaScript' === "JavaScript",
+// 'JavaScript' === `JavaScript`,
+// "JavaScript" === `JavaScript`,
+// )
+
+// console.log(new String('JavaScript'),
+// new String("JavaScript"),
+// new String(`JavaScript`),
+// )
+
+let counter = 0;
+console.time("performance");
+while (counter < 100000) {
+    "JavaScript";
+    counter++;
+}
+console.timeEnd("performance");
+
+
+
+
+// console.log("Each constructor is a function that has a property named \"prototype\" that is used to implement prototype-based inheritance and shared properties.");
+// console.log('An object\'s prototype chain should have finite length.');
+// console.log(`The proper way to cause a line terminator code point to be part of the String value of a string literal is to use an escape sequence such as \\n or \\u000A`);
+
+
+
+
+// let daysOfWeek = "0 - Sun\n1 - Mon\n2 - Tue\n3 - Wed\n4 - Thu\n5 - Fri\n6 - Sat";
+// console.log(daysOfWeek);
+
+
+
+
+
+// let daysOfWeekInCodePoints = "\u0030 \u002d \u0053\u0075\u006e\u000A\u0031 \u002d \u004d\u006f\u006e\u000A\u0032 \u002d \u0054\u0075\u0065\u000A\u0033 \u002d \u0057\u0065\u0064\u000A\u0034 \u002d \u0054\u0068\u0075\u000A\u0035 \u002d \u0046\u0072\u0069\u000A\u0036 \u002d \u0053\u0061\u0074";
+// console.log(daysOfWeekInCodePoints);
+

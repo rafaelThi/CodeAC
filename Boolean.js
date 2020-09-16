840528a529fc68b3740b0b72cd40de426cc70939
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-16 14:26:39 -0300

    AULAS AGILECODE

---
+++ Boolean.js
@@ -0,0 +1,64 @@
+// console.log(
+//     true,
+//     false
+// )
+
+// console.log(
+//     new Boolean(true),
+// new Boolean(false),
+
+// )
+
+//     let condition1_0 = true;
+// if (condition1_0) {
+//     console.log("The condition is true");
+// } else {
+//     console.log("The condition is false");
+// }
+
+
+// let condition1_1 = false;
+// if (condition1_1) {
+//     console.log("The condition is true");
+// } else {
+//     console.log("The condition is false");
+// }
+
+
+// let condition2_1 = new Boolean(true);
+// if (condition2_1) {
+//     console.log("The condition is true");
+// } else {
+//     console.log("The condition is false");
+// }
+
+
+
+// let condition2_2 = new Boolean(false);
+// if (condition2_2) {
+//     console.log("The condition is true");
+// } else {
+//     console.log("The condition is false");
+// }
+
+
+// console.log(
+// !!0,
+// !!NaN,
+// !!"",
+// !!false,
+// !!undefined,
+// !!null,
+
+// )
+
+console.log(
+    !!-10,
+!!"JavaScript",
+!!{},
+!![],
+!!/JavaScript/,
+!!new Date(),
+!!function () {},
+
+)

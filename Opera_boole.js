6488e3274dbdaced97949a63d3cebdd73fcc6db5
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-16 14:47:19 -0300

    AULAS AGILECODE

---
+++ Opera_boole.js
@@ -0,0 +1,104 @@
+console.log(
+    1 == 2,
+10 === 10,
+'a' != 'b',
+3 !== 2,
+6 > 7,
+5 >= 5,
+'z' < 'x',
+'c' <= 'c',
+
+)
+
+console.log(
+    0 == '',
+0 == '0' ,
+false == undefined,
+false == null,
+null == undefined,
+1 == true,
+0 == false,
+0 == '\n',
+)
+
+console.log(
+    0 === '',
+0 === '0' ,
+false === undefined,
+false === null,
+null === undefined,
+1 === true,
+0 === false,
+0 === '\n',
+
+)
+
+console.log(
+    0 || 2,
+1 || 2,
+1 && 2,
+0 && 2,
+
+)
+
+
+
+// function generateSerial (max) {
+//     return Math.floor(Math.random() * max);
+// }
+
+// generateSerial(10);
+// generateSerial(100);
+// generateSerial(1000);
+// generateSerial();
+
+
+
+
+
+// function generateSerial (max) {
+//     if (max === undefined) {
+//         max = 1000;
+//     }
+//     return Math.floor(Math.random() * max);
+// }
+
+// generateSerial(10);
+// generateSerial(100);
+// generateSerial(1000);
+// generateSerial();
+
+
+
+
+// function generateSerial (max) {
+//     if (!max) {
+//         max = 1000;
+//     }
+//     return Math.floor(Math.random() * max);
+// }
+
+// generateSerial(10);
+// generateSerial(100);
+// generateSerial(1000);
+// generateSerial();
+
+
+
+
+
+function generateSerial (max) {
+    max = max || 1000;
+    return Math.floor(Math.random() * max);
+}
+
+generateSerial(10);
+generateSerial(100);
+generateSerial(1000);
+generateSerial();
+
+
+console.log(
+(10) ? 'good' : 'bad',
+(0) ? 'good' : 'bad',
+)

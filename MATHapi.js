c835a3767f8be5d06d24b3fb7350bfef5b9b4733
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-16 09:22:16 -0300

    AULAS AGILECODE

---
+++ MATHapi.js
@@ -0,0 +1,46 @@
+// console.log(
+//     Math.E,
+// Math.LN10,
+// Math.LN2,
+// Math.LOG10E,
+// Math.LOG2E,
+// Math.PI,
+// Math.SQRT1_2,
+// Math.SQRT2,
+
+// )
+
+// console.log(
+//     Math.abs(10),
+// Math.abs(-10),
+// Math.ceil(1.1),
+// Math.ceil(-1.1),
+// Math.floor(9.9),
+// Math.floor(-9.9),
+// Math.round(4.5),
+// Math.round(-4.5),
+// Math.sign(5),
+// Math.sign(-5),
+// Math.trunc(2.3),
+// Math.trunc(-2.3),
+
+// )
+// console.log(
+//     Math.cbrt(8),
+// Math.cos(Math.PI/3),
+// Math.exp(1),
+// Math.hypot(3,4),
+// Math.log(1),
+// Math.pow(2,10),
+// Math.sin(Math.PI/2),
+// Math.sqrt(4),
+// Math.tan(Math.PI/4),
+
+// )
+
+// console.log(
+//     Math.min(1,2,3,4,5,6),
+// Math.max(1,2,3,4,5,6),
+// Math.random(),
+
+// )

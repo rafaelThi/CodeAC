917af339e6fc3a8f93499337910d1df146bf6767
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-16 10:25:28 -0300

    AULAS AGILECODE

---
+++ Template_Literal.js
@@ -0,0 +1,71 @@
+// let host = "localhost";
+// let port = "3000";
+// let resource = "users";
+// let url = "https://" + host + ":" + port + "/" + resource;
+// console.log(url);
+
+
+
+// let host = "localhost";
+// let port = "3000";
+// let resource = "users";
+// let url = `https://${host}:${port}/${resource}`;
+// console.log(url);
+
+
+// let monthsOfYear = "0 - Jan" + 
+// "1 - Feb" + 
+// "2 - Mar" + 
+// "3 - Apr" + 
+// "4 - May" + 
+// "5 - Jun" +
+// "6 - Jul" +
+// "7 - Aug" +
+// "8 - Sep" +
+// "9 - Oct" + 
+// "10 - Nov" +
+// "11 - Dec"
+// console.log(monthsOfYear);
+
+// let monthsOfYear = "0 - Jan\
+// 1 - Feb\
+// 2 - Mar\
+// 3 - Apr\
+// 4 - May\
+// 5 - Jun\
+// 6 - Jul\
+// 7 - Aug\
+// 8 - Sep\
+// 9 - Oct\
+// 10 - Nov\
+// 11 - Dec";
+// console.log(monthsOfYear);
+
+
+// let monthsOfYear = "0 - Jan\n\
+// 1 - Feb\n\
+// 2 - Mar\n\
+// 3 - Apr\n\
+// 4 - May\n\
+// 5 - Jun\n\
+// 6 - Jul\n\
+// 7 - Aug\n\
+// 8 - Sep\n\
+// 9 - Oct\n\
+// 10 - Nov\n\
+// 11 - Dec";
+// console.log(monthsOfYear);
+
+let monthsOfYear = `0 - Jan
+1 - Feb
+2 - Mar
+3 - Apr
+4 - May
+5 - Jun
+6 - Jul
+7 - Aug
+8 - Sep
+9 - Oct
+10 - Nov
+11 - Dec`;
+console.log(monthsOfYear);

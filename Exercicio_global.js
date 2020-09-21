084e74d6145ca51990b99f8881b3f157c6a3bc25
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-21 13:45:21 -0300

    Exercicio

--- Exercicio_global.js
+++ Exercicio_global.js
@@ -4,14 +4,26 @@ let busca = command.match( regexp )
 let tableName = busca[1]
 
 let columns = command.match(regexp)
-columns = columns[2].split(",");
+columns = columns[2].split(", ");
 
 const database = {
     "tables": {
         [tableName]:{
-            columns
-        },
+            columns:{},
         data: []
+        }
     }
+};
+
+for (column of columns){
+    coluns = column.split(" ");
+    const name = coluns[0];
+    const type = coluns[1];
+
+    console.log (name, type)
+
+ database.tables[tableName].columns[name]=type;
+
 }
-console.log(JSON.stringify(database));
+
+ console.log(JSON.stringify(database, undefined, " "));

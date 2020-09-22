e7342a9fbf45339010d1d7db6b52e625ac1a0f4f
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-22 12:03:13 -0300

    EXERCICIO 3

--- Exercicio_global.js
+++ Exercicio_global.js
@@ -1,29 +1,39 @@
-let command = "create table author (id number, name string, age number, city string, state string, country string)";
-let regexp = /create table (\w+) \((.+)\)/;
-let busca = command.match( regexp )
-let tableName = busca[1]
+
+const database = {
+    tables: {},
+        createTable(command){
+                    
+                    let regexp = /create table (\w+) \((.+)\)/;
+                    let busca = command.match( regexp )
+                    let tableName = busca[1]
+                   this.tables[tableName] = {
+                        columns:{},
+            
+                    data: []
+                    }
 
 let columns = command.match(regexp)
 columns = columns[2].split(", ");
-
-const database = {
-    "tables": {
-        [tableName]:{
-            columns:{},
-        data: []
-        }
-    }
-};
 
 for (column of columns){
     coluns = column.split(" ");
     const name = coluns[0];
     const type = coluns[1];
 
-    console.log (name, type)
+    // console.log (name, type)
 
- database.tables[tableName].columns[name]=type;
+ this.tables[tableName].columns[name]=type;
 
 }
+        
+    },
 
- console.log(JSON.stringify(database, undefined, " "));
+        execute(command){
+            if (command.startsWith("create table")){
+            return this.createTable(command)
+            }
+        }
+};
+
+database.createTable("create table author (id number, name string, age number, city string, state string, country string)");
+ console.log(JSON.stringify(database, undefined, "   "));

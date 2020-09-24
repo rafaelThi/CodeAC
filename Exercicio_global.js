8c0ded8e71bd423d389de60ce8882f44462a9887
Rafael de Melo <rafael.pereira20@icloud.com>
2020-09-24 14:32:04 -0300

    EXERCICIO 5

--- Exercicio_global.js
+++ Exercicio_global.js
@@ -33,11 +33,39 @@ for (column of columns){
 }
         
     },
+        insert(command){
 
+            const regexp = /insert into (\w+) \((.+)\) values \((.+)\)/; 
+            const parteCommand = command.match(regexp);
+            let tableName = parteCommand[1]
+            let columns = parteCommand[2]
+            let values = parteCommand[3]
+
+            columns = columns.split(", ");
+            values = values.split(", ");
+
+            let row = {}
+            for (let i = 0; i < columns.length; i++){
+                const colun = columns[i];
+                const value = values[i];
+                row[colun] = value                
+            }
+
+            this.tables[tableName].data.push(row);
+            console.log(row)
+            // console.log(tableName, columns, values)
+            // console.log(command)
+
+        },
         execute(command){
             if (command.startsWith("create table")){
             return this.createTable(command)
             }
+
+
+            if (command.startsWith("insert")){
+                return this.insert(command)
+                }
 
             //
             const message = `Syntax error: "${command}`
@@ -47,10 +75,20 @@ for (column of columns){
         }
 };
 try {
-database.createTable("create table author (id number, name string, age number, city string, state string, country string)");
-//
+
+
+    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+
+
+    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+
+
+// database.createTable("create table author (id number, name string, age number, city string, state string, country string)");
+// //
 // database.execute("select id, name from author")
-//
+// //
 console.log(JSON.stringify(database, undefined, "   "));
 } catch(e) {
     console.error(e.message)
